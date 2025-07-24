aa2082ec7abb4f7cc513cf1b7d33e6b1d1cf46d4
awhomadh <aerphan@gmail.com>
2025-07-24 23:59:09 +0500

    setting up

--- include/header.php
+++ include/header.php
@@ -124,3 +124,24 @@
         <!-- Page Title -->
         <title>Dashboard | Dashly</title>
     </head>
+        
+    <body>
+
+            <!-- THEME CONFIGURATION -->
+            <script>
+                let themeAttrs = document.documentElement.dataset;
+            
+                for(let attr in themeAttrs) {
+                    if(localStorage.getItem(attr) != null) {
+                        document.documentElement.dataset[attr] = localStorage.getItem(attr);
+            
+                        if (theme === 'auto') {
+                            document.documentElement.dataset.theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+            
+                            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+                                e.matches ? document.documentElement.dataset.theme = 'dark' : document.documentElement.dataset.theme = 'light';
+                            });
+                        }
+                    }
+                }
+            </script>
